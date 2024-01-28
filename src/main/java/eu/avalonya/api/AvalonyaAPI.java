82b9636aca408d26e1d0362605deafb73debcc74
Alexandre Barde <alexandre@brde.fr>
2024-01-28 18:30:06 -0500

    Ajout du code de base pour l'API

---
+++ src/main/java/eu/avalonya/api/AvalonyaAPI.java
@@ -0,0 +1,20 @@
+package eu.avalonya.api;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AvalonyaAPI extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+
+    }
+
+}
