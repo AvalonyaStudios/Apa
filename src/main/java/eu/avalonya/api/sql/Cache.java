af11735d8feace1b1d3514b7043b0f84194d4561
Alexandre Barde <19241664+AlexandreBarde@users.noreply.github.com>
2024-02-29 22:26:04 -0500

    [AVA-77] Add player to staff cache (#16)

--- src/main/java/eu/avalonya/api/sql/Cache.java
+++ src/main/java/eu/avalonya/api/sql/Cache.java
@@ -2,6 +2,7 @@ package eu.avalonya.api.sql;
 
 import eu.avalonya.api.models.AvalonyaPlayer;
 import eu.avalonya.api.models.Rank;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,5 +13,10 @@ public class Cache
 {
     public static HashMap<UUID, AvalonyaPlayer> avaloniaPlayers = new HashMap<UUID, AvalonyaPlayer>();
     public static HashMap<Rank, List<String>> permissions = new HashMap<>();
-    public static ArrayList<String> staffList = new ArrayList<String>();
+
+    /**
+     * Pour le moment on n'a pas besoin de stocker un AvalonyaPlayer, un Player spigot suffit.
+     * Car on s'en sert uniquement pour envoyer un message, à voir dans le temps si on a besoin de plus.
+     */
+    public static ArrayList<Player> staffList = new ArrayList<>();
 }
